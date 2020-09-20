fc0138d68218979098b8666586f6d1de5f791bb8
AdemirDeSousa <jadsqj@gmail.com>
2020-09-20 19:36:28 -0300

    Finalizando app React Native

--- front-mobile/src/pages/CreateRecord/index.tsx
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -1,16 +1,136 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, View, TextInput, Alert } from 'react-native';
+import PlatformCard from './PlatformCard';
+import { GamePlatform, Game } from './types';
+import RNPickerSelect from 'react-native-picker-select';
 import Header from '../../components/Header';
+import { FontAwesome5 as Icon } from '@expo/vector-icons';
+import axios from 'axios';
+import { RectButton } from 'react-native-gesture-handler';
+
+const placeHolder = {
+  label: 'Selecione o game',
+  value: null
+}
+
+const BASE_URL = 'http://192.168.15.2:8080';
+
+const mapSelectValues = (games: Game[]) => {
+  return games.map(game => ({
+    ...game,
+    label:game.title,
+    value: game.id
+  }));
+}
 
 const CreateRecord = () => {
-    return (
-        <>
-            <Header />
-            <Text>Hello</Text>  
-        </>
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [platform, setPlatform] = useState<GamePlatform>();
+  const [selectedGame, setSelectedGame] = useState('');
+  const [allGames, setAllGames] = useState<Game[]>([]);
+  const [filteredGames, setFilteredGames ] = useState<Game[]>([]);
+
+  const handleChangePlatform = (selectedPlatform: GamePlatform) => {
+    setPlatform(selectedPlatform);
+    const gamesByPlatform = allGames.filter(
+      game => game.platform === selectedPlatform
+    );
+    setFilteredGames(gamesByPlatform);
+  }
+
+  const handleSubmit = () => {
+    const payload = { name, age, game_id: selectedGame };
+
+    axios.post(`${BASE_URL}/records`, payload)
+      .then(() => {
+        Alert.alert('Dados salvos com sucesso!');
+        setName('');
+        setAge('');
+        setSelectedGame('');
+        setPlatform(undefined);
+      }).catch(() => Alert.alert('Erro ao salvar informações'));
+  }
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/games`)
+      .then( response => {
+        const selectValue = mapSelectValues(response.data);
+
+        setAllGames(selectValue);
+
+      }).catch(() => Alert.alert('Erro ao listar os jogos'));
+  }, []);
+
+  return (
+      <>
+        <Header />
+        <View style={styles.container}>
+          <TextInput style={styles.inputText} placeholder="Nome" placeholderTextColor="#9E9E9E" onChangeText={text => setName(text)} value={name}/>
+          <TextInput style={styles.inputText} placeholder="Idade" placeholderTextColor="#9E9E9E" keyboardType="numeric" maxLength={3} onChangeText={text => setAge(text)} value={age}/> 
+          <View style={styles.platformContainer}>
+            <PlatformCard platform="PC" icon="laptop" onChange={handleChangePlatform} activePlatform={platform}/>
+            <PlatformCard platform="XBOX" icon="xbox" onChange={handleChangePlatform} activePlatform={platform}/>
+            <PlatformCard platform="PLAYSTATION" icon="playstation" onChange={handleChangePlatform} activePlatform={platform}/>
+          </View>
+          <RNPickerSelect placeholder={placeHolder}
+          onValueChange={value => {setSelectedGame(value)}} 
+          style={pickerSelectStyles}
+          Icon={() => {
+            return <Icon name="chevron-down" color="#9E9E9E" size={25}/>
+          }}
+          items={filteredGames}
+          value={selectedGame}
+          />
+          <View style={styles.footer}>
+            <RectButton style={styles.button} onPress={handleSubmit}>
+              <Text style={styles.buttonText}>
+                Salvar
+              </Text>
+            </RectButton>
+          </View>
+        </View> 
+      </>
     );
 }
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    color: '#ED7947',
+    paddingRight: 30,
+    fontFamily: "Play_700Bold",
+    height: 50
+  },
+
+  inputAndroid: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    color: '#ED7947',
+    paddingRight: 30,
+    fontFamily: "Play_700Bold",
+    height: 50
+  },
+
+  placeholder: {
+    color: '#9E9E9E',
+    fontSize: 16,
+    fontFamily: "Play_700Bold",
+  },
+
+  iconContainer: {
+    top: 10,
+    right: 12,
+  }
+});
 
 const styles = StyleSheet.create({
     container: {
